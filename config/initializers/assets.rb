439e751e467195f7de4b2dcfc20dec80a5a8505c
TruongLe <htr.lee@gmail.com>
2017-01-21 01:49:28 +0000

    fix for deployment

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,12 @@ Rails.application.config.assets.compile = true
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile =  ['*.js', '*.css', 'jquery-migrate-rails.js'] 
+Rails.application.config.assets.precompile += %w( application-admin.js )
+Rails.application.config.assets.precompile += %w( application-index.js )
+Rails.application.config.assets.precompile += %w( application-shared.js )
+
+Rails.application.config.assets.precompile += %w( application-admin.css )
+Rails.application.config.assets.precompile += %w( application-index.css )
+Rails.application.config.assets.precompile += %w( application-shared.css )
