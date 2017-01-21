c6cc855703d8ac46f38c8e129779de9414925b58
TruongLe <htr.lee@gmail.com>
2017-01-21 05:07:14 +0000

    fix undefine jquery functions

--- app/assets/javascripts/adminscript/light-bootstrap-dashboard.js
+++ app/assets/javascripts/adminscript/light-bootstrap-dashboard.js
@@ -9,6 +9,100 @@ var navbar_initialized = false;
 $(document).ready(function(){
     window_width = $(window).width();
     
+    var lbd = {
+        misc:{
+            navbar_menu_visible: 0
+        },
+        
+        checkSidebarImage: function(){
+            $sidebar = $('.sidebar');
+            image_src = $sidebar.data('image');
+            
+            if(image_src !== undefined){
+                sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
+                $sidebar.append(sidebar_container);
+            }  
+        },
+        initRightMenu: function(){  
+             if(!navbar_initialized){
+                $navbar = $('nav').find('.navbar-collapse').first().clone(true);
+                
+                $sidebar = $('.sidebar');
+                sidebar_color = $sidebar.data('color');
+                
+                $logo = $sidebar.find('.logo').first();
+                logo_content = $logo[0].outerHTML;
+                        
+                ul_content = '';
+                 
+                $navbar.attr('data-color',sidebar_color);
+                 
+                // add the content from the sidebar to the right menu
+                content_buff = $sidebar.find('.nav').html();
+                ul_content = ul_content + content_buff;
+                
+                //add the content from the regular header to the right menu
+                $navbar.children('ul').each(function(){
+                    content_buff = $(this).html();
+                    ul_content = ul_content + content_buff;   
+                });
+                 
+                ul_content = '<ul class="nav navbar-nav">' + ul_content + '</ul>';
+                
+                navbar_content = logo_content + ul_content;
+                
+                $navbar.html(navbar_content);
+                 
+                $('body').append($navbar);
+                 
+                background_image = $sidebar.data('image');
+                if(background_image != undefined){
+                    $navbar.css('background',"url('" + background_image + "')")
+                           .removeAttr('data-nav-image')
+                           .addClass('has-image');                
+                }
+                 
+                 
+                 $toggle = $('.navbar-toggle');
+                 
+                 $navbar.find('a').removeClass('btn btn-round btn-default');
+                 $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
+                 $navbar.find('button').addClass('btn-simple btn-block');
+                
+                 $toggle.click(function (){    
+                    if(lbd.misc.navbar_menu_visible == 1) {
+                        $('html').removeClass('nav-open'); 
+                        lbd.misc.navbar_menu_visible = 0;
+                        $('#bodyClick').remove();
+                         setTimeout(function(){
+                            $toggle.removeClass('toggled');
+                         }, 400);
+                    
+                    } else {
+                        setTimeout(function(){
+                            $toggle.addClass('toggled');
+                        }, 430);
+                        
+                        div = '<div id="bodyClick"></div>';
+                        $(div).appendTo("body").click(function() {
+                            $('html').removeClass('nav-open');
+                            lbd.misc.navbar_menu_visible = 0;
+                            $('#bodyClick').remove();
+                             setTimeout(function(){
+                                $toggle.removeClass('toggled');
+                             }, 400);
+                        });
+                       
+                        $('html').addClass('nav-open');
+                        lbd.misc.navbar_menu_visible = 1;
+                        
+                    }
+                });
+                navbar_initialized = true;
+            }
+       
+        }
+    }
     // check if there is an image set for the sidebar's background
     lbd.checkSidebarImage();
     
@@ -44,100 +138,7 @@ $(window).resize(function(){
     }
 });
     
-lbd = {
-    misc:{
-        navbar_menu_visible: 0
-    },
-    
-    checkSidebarImage: function(){
-        $sidebar = $('.sidebar');
-        image_src = $sidebar.data('image');
-        
-        if(image_src !== undefined){
-            sidebar_container = '<div class="sidebar-background" style="background-image: url(' + image_src + ') "/>'
-            $sidebar.append(sidebar_container);
-        }  
-    },
-    initRightMenu: function(){  
-         if(!navbar_initialized){
-            $navbar = $('nav').find('.navbar-collapse').first().clone(true);
-            
-            $sidebar = $('.sidebar');
-            sidebar_color = $sidebar.data('color');
-            
-            $logo = $sidebar.find('.logo').first();
-            logo_content = $logo[0].outerHTML;
-                    
-            ul_content = '';
-             
-            $navbar.attr('data-color',sidebar_color);
-             
-            // add the content from the sidebar to the right menu
-            content_buff = $sidebar.find('.nav').html();
-            ul_content = ul_content + content_buff;
-            
-            //add the content from the regular header to the right menu
-            $navbar.children('ul').each(function(){
-                content_buff = $(this).html();
-                ul_content = ul_content + content_buff;   
-            });
-             
-            ul_content = '<ul class="nav navbar-nav">' + ul_content + '</ul>';
-            
-            navbar_content = logo_content + ul_content;
-            
-            $navbar.html(navbar_content);
-             
-            $('body').append($navbar);
-             
-            background_image = $sidebar.data('image');
-            if(background_image != undefined){
-                $navbar.css('background',"url('" + background_image + "')")
-                       .removeAttr('data-nav-image')
-                       .addClass('has-image');                
-            }
-             
-             
-             $toggle = $('.navbar-toggle');
-             
-             $navbar.find('a').removeClass('btn btn-round btn-default');
-             $navbar.find('button').removeClass('btn-round btn-fill btn-info btn-primary btn-success btn-danger btn-warning btn-neutral');
-             $navbar.find('button').addClass('btn-simple btn-block');
-            
-             $toggle.click(function (){    
-                if(lbd.misc.navbar_menu_visible == 1) {
-                    $('html').removeClass('nav-open'); 
-                    lbd.misc.navbar_menu_visible = 0;
-                    $('#bodyClick').remove();
-                     setTimeout(function(){
-                        $toggle.removeClass('toggled');
-                     }, 400);
-                
-                } else {
-                    setTimeout(function(){
-                        $toggle.addClass('toggled');
-                    }, 430);
-                    
-                    div = '<div id="bodyClick"></div>';
-                    $(div).appendTo("body").click(function() {
-                        $('html').removeClass('nav-open');
-                        lbd.misc.navbar_menu_visible = 0;
-                        $('#bodyClick').remove();
-                         setTimeout(function(){
-                            $toggle.removeClass('toggled');
-                         }, 400);
-                    });
-                   
-                    $('html').addClass('nav-open');
-                    lbd.misc.navbar_menu_visible = 1;
-                    
-                }
-            });
-            navbar_initialized = true;
-        }
-   
-    }
-}
+
 
 
 // Returns a function, that, as long as it continues to be invoked, will not
